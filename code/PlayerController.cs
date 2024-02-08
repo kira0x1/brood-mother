0ec097d0f8cac319f11556bb6e68d611bf58b50f
Kira <kira.eva@protonmail.com>
2024-02-08 18:53:52 +0000

    Animate with movement

--- code/PlayerController.cs
+++ code/PlayerController.cs
@@ -1,13 +1,35 @@
 using Sandbox;
+using Sandbox.Citizen;
 
 public sealed class PlayerController : Component
 {
-    [Property] public float TurnSpeed { get; set; } = 100f;
-    [Property] public float MoveSpeed { get; set; } = 50f;
+    [Property] public float TurnSpeed { get; set; } = 500f;
+    [Property] public float MoveSpeed { get; set; } = 80f;
+
+    private CharacterController controller;
+    private CitizenAnimationHelper animHelper;
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        animHelper = GameObject.Components.Get<CitizenAnimationHelper>();
+        controller = GameObject.Components.Get<CharacterController>();
+    }
 
     protected override void OnUpdate()
     {
-        Transform.Position += Input.AnalogMove * MoveSpeed * Time.Delta;
-        Transform.Rotation += Input.AnalogLook.ToRotation() * TurnSpeed;
+        float turnAxis = Input.AnalogLook.yaw * TurnSpeed * Time.Delta;
+        Transform.LocalRotation = Transform.LocalRotation.RotateAroundAxis(Vector3.Up, turnAxis);
+
+        var velocity = Input.AnalogMove.Normal * MoveSpeed;
+        controller.Velocity = velocity;
+        controller.Move();
+
+        UpdateAnim(velocity);
+    }
+
+    private void UpdateAnim(Vector3 velocity)
+    {
+        animHelper.WithVelocity(velocity);
     }
 }
